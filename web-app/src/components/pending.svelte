5d59f86571c2e008d53c7a823056e9baf1f3ae2a
bwty <email-me@belsy.space>
2019-08-25 10:55:09 +0200

    wallet stauts

--- web-app/src/components/pending.svelte
+++ web-app/src/components/pending.svelte
@@ -11,7 +11,29 @@
     <img alt="Transit" class="logo-img" src="logo_invert.png">
 </header>
 
+
+
 <section class="d-flex flex-column action-section">
+{#if $wallet.status == 'Locked'}
+<div style="text-align:center">
+    <button on:click="{wallet.unlock}"> Connect your wallet</button>
+</div>
+{:else if $wallet.status == 'Unlocking'}
+<div style="text-align:center">
+    <h3> Please accept the connection request </h3>
+</div>
+{:else if $wallet.status == 'WrongChain'}
+<div style="text-align:center">
+    <h3> Please change your network </h3>
+</div>
+
+{:else if $wallet.status == 'NoWallet'}
+
+<div style="text-align:center">
+    <h3> You need a wallet darling! </h3>
+</div>
+
+{:else if $wallet.status == 'Ready'}
 
         <h3>🤹‍♂️ {message}</h3>
         <div class="progress">
@@ -24,6 +46,7 @@
             </div>
         </div>
 
+{/if}
 </section>
 
 <footer>
